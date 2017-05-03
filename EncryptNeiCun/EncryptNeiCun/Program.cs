defb6884cbb2abeab0b7a32780058e3739235454
yuliang <109827298@qq.com>
2017-05-03 15:20:07 +0800

    add fiels

---
+++ EncryptNeiCun/EncryptNeiCun/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptNeiCun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nKey = 123456777;
+
+            int ValueA = 999888;
+
+            int nAfterEncrypted = ValueA ^ nKey;
+
+            Console.WriteLine(nAfterEncrypted);
+
+            int nDeEncrypted = nAfterEncrypted ^ nKey;
+
+            Console.WriteLine(nDeEncrypted);
+        }
+    }
+}
