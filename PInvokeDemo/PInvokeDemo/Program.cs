56508ba82254eaad0f2fe6d7a957ccc443d29aab
yuliang <109827298@qq.com>
2017-09-11 16:33:04 +0800

    add files

---
+++ PInvokeDemo/PInvokeDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace PInvokeDemo
+{
+    class Program
+    {
+        [DllImportAttribute("user32.dll", EntryPoint = "SetCursorPos")]
+        [return: MarshalAsAttribute(UnmanagedType.Bool)]
+        public static extern bool SetCursorPos(int x, int y);
+
+        static void Main(string[] args)
+        {
+            Random r = new Random(unchecked((int)DateTime.Now.Ticks));
+
+            int i = 1;
+
+            do
+            {
+                int x = r.Next(0, 100);
+                int y = r.Next(0, 100);
+
+                SetCursorPos(x, y);
+
+                i++;
+
+                Thread.Sleep(300);
+            }
+            while (i < 10);
+
+            Console.ReadKey();
+        }
+    }
+}
