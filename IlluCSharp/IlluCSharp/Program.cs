24c782e41a6e9781900fabe4cd6f792aa6a3dac9
YuLiang <109827298@qq.com>
2017-09-17 10:52:04 +0800

    add

---
+++ IlluCSharp/IlluCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlluCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 5;
+            {
+                int var1 = 10;
+            }
+        }
+    }
+}
