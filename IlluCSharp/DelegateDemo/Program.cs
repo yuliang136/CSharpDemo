0bfa918eb035760935d169770266b21423e11361
yuliang <109827298@qq.com>
2017-09-19 11:02:35 +0800

    add files

--- IlluCSharp/DelegateDemo/Program.cs
+++ IlluCSharp/DelegateDemo/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace DelegateDemo
 {
+    public delegate void Print();
+
     class Program
     {
+        public static void PrintMethod1()
+        {
+            Console.WriteLine("First Method");
+        }
+
+        public static void PrintMethod2()
+        {
+            Console.WriteLine("Second Method");
+        }
+
         static void Main(string[] args)
         {
+            Print print = PrintMethod1;
+
+            print += PrintMethod2;
+
+            //print();
+
+            print -= PrintMethod1;
+
+            print();
+
         }
     }
 }
