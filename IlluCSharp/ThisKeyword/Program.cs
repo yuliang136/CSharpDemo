ee88589ac78543742b2e28623fb8d2616dbd4fa5
yuliang <109827298@qq.com>
2017-09-20 11:42:37 +0800

    add files

---
+++ IlluCSharp/ThisKeyword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisKeyword
+{
+    class MyClass
+    {
+        int Var1 = 10;
+
+        public int ReturnMaxSum(int Var1)
+        {
+            return Var1 > this.Var1 ? Var1 : this.Var1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+
+            Console.WriteLine("Max: {0}", mc.ReturnMaxSum(30));
+            Console.WriteLine("Max: {0}", mc.ReturnMaxSum(5));
+        }
+    }
+}
