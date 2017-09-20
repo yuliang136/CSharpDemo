5d20bca35c5f4bd37e2fbc7ef8ae22dffcfa102e
yuliang <109827298@qq.com>
2017-09-20 19:16:42 +0800

    add ModifiersOnAccessors

---
+++ IlluCSharp/ModifiersOnAccessors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModifiersOnAccessors
+{
+    class Person
+    {
+        public string Name { get; private set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Capt. Ernest Evans");
+
+            Console.WriteLine("Person's name is {0}", p.Name);
+
+            //p.Name = "11";
+        }
+    }
+}
