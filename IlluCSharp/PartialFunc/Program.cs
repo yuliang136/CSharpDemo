c0167ffd1d832aec8e8932c9f840dfceb6e0d8e5
YuLiang <109827298@qq.com>
2017-09-20 23:21:47 +0800

    add files

---
+++ IlluCSharp/PartialFunc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialFunc
+{
+    partial class MyClass
+    {
+        partial void PrintSum(int x, int y);
+
+        public void Add(int x, int y)
+        {
+            PrintSum(x, y);
+        }
+    }
+
+    partial class MyClass
+    {
+        partial void PrintSum(int x, int y)
+        {
+            Console.WriteLine("Sum is {0}", x + y);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mc = new MyClass();
+
+            mc.Add(5, 611);
+        }
+    }
+}
