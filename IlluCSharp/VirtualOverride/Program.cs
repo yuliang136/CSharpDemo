acb72f2261dc5ab08b95a1854ea8981f8664aba0
yuliang <109827298@qq.com>
2017-09-22 14:05:57 +0800

    aa

---
+++ IlluCSharp/VirtualOverride/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualOverride
+{
+    class MyBaseClass
+    {
+        virtual public void Print()
+        {
+            Console.WriteLine("This is the base class.");
+        }
+    }
+
+    class MyDerivedClass:MyBaseClass
+    {
+        override public void Print()
+        {
+            Console.WriteLine("This is the derived class.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass derived = new MyDerivedClass();
+
+            MyBaseClass mybc = (MyBaseClass)derived;
+
+            derived.Print();
+            mybc.Print();
+
+        }
+    }
+}
