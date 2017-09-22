3563cb739c8f0fb19dd75ced3f042b2a119ebed6
yuliang <109827298@qq.com>
2017-09-22 14:55:27 +0800

    add

---
+++ IlluCSharp/OverridingMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverridingMethod
+{
+    class MyBaseClass
+    {
+        virtual public void Print()
+        {
+            Console.WriteLine("This is the base class.");
+        }
+    }
+
+    class MyDerivedClass : MyBaseClass
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is the derived class.");
+        }
+    }
+
+    class SecondDerived : MyDerivedClass
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is the second derived class.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SecondDerived derived = new SecondDerived();
+
+            MyBaseClass mybc = (MyBaseClass)derived;
+
+            derived.Print();
+
+            mybc.Print();
+        }
+    }
+}
