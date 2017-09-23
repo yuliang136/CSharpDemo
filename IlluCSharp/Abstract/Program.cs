7a00ef1ea44ec9a91344d289b7c3e1e6a5243949
yuliang <109827298@qq.com>
2017-09-23 18:52:55 +0800

    add files

---
+++ IlluCSharp/Abstract/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    abstract class MyBase
+    {
+        public int SideLength = 10;
+        const int TriangleSideCount = 3;
+
+        abstract public void PrintStuff(string s);
+        abstract public int MyInt { get; set; }
+
+        public int PerimeterLength()
+        {
+            return TriangleSideCount * SideLength;
+        }
+
+    }
+
+    class MyClass : MyBase
+    {
+        public override void PrintStuff(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        private int _myInt;
+
+        public override int MyInt
+        {
+            get
+            {
+                return _myInt;
+            }
+            set
+            {
+                _myInt = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            mc.PrintStuff("This is a string.");
+            mc.MyInt = 28;
+
+            Console.WriteLine(mc.MyInt);
+            Console.WriteLine("Perimeter Length: {0}",mc.PerimeterLength());
+        }
+    }
+}
---
+++ IlluCSharp/AbstractDemo1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDemo1
+{
+    abstract class AbClass
+    {
+        public void IdentifyBase()
+        {
+            Console.WriteLine("I am AbClass");
+        }
+
+        abstract public void IdentifyDerived();
+    }
+
+    class DerivedClass : AbClass
+    {
+        public override void IdentifyDerived()
+        {
+            Console.WriteLine("I am DerivedClass");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AbClass a = new AbClass();
+
+            DerivedClass b = new DerivedClass();
+            b.IdentifyBase();
+            b.IdentifyDerived();
+        }
+    }
+}
