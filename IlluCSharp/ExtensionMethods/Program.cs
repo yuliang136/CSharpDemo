966ffe458557cc4a1b23469a00d234c03cf6dafd
yuliang <109827298@qq.com>
2017-09-25 10:51:13 +0800

    add

---
+++ IlluCSharp/ExtensionMethods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    sealed class MyData
+    {
+        private double D1;
+        private double D2;
+        private double D3;
+
+        public MyData(double d1, double d2, double d3)
+        {
+            D1 = d1;
+            D2 = d2;
+            D3 = d3;
+        }
+
+        public double Sum()
+        {
+            return D1 + D2 + D3;
+        }
+    }
+
+    static class ExtendMyData
+    {
+        public static double Average(this MyData md)
+        {
+            return md.Sum() / 3;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyData md = new MyData(3, 4, 5);
+            Console.WriteLine("Sum: {0}", md.Sum());
+            Console.WriteLine("Average: {0}", md.Average());
+        }
+    }
+}
