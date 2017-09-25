ce7052512a944a885c9bf2caae5f67181dc57d22
yuliang <109827298@qq.com>
2017-09-25 16:46:35 +0800

    add fiels

---
+++ IlluCSharp/RelationalAndEquiality/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationalAndEquiality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 5, y = 4;
+
+            //Console.WriteLine("x == x is {0}", x == x);
+            //Console.WriteLine("x == y is {0}", x == y);
+
+            //int i = -4;
+
+            //int a = i >> 1;
+
+            //Console.WriteLine("a =  {0}", a);
+
+            //Console.WriteLine(Convert.ToInt32("11111111111111111111111111111100", 2));
+
+            //Console.WriteLine(Convert.ToInt32("11111111111111111111111111111110", 2));
+
+            Console.WriteLine(Convert.ToString(-4, 2));
+        }
+    }
+}
