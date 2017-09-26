e3b81a56a10db08d9d02dc9784afcdd84236f6e3
yuliang <109827298@qq.com>
2017-09-26 16:22:06 +0800

    add files

---
+++ IlluCSharp/GotoStatement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GotoStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //{
+            //    goto HaHa;
+            //}
+            {
+            HaHa: Console.WriteLine("Here");
+            int i = 50;
+            }
+
+            goto HaHa;
+            
+
+
+
+            Console.WriteLine("Before");
+
+
+
+
+
+            Console.WriteLine("Over");
+        }
+    }
+}
