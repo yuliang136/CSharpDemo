fe1afbb04c1086d4f07351fbce549716168f4bc3
yuliang <109827298@qq.com>
2017-09-26 16:49:56 +0800

    add files

---
+++ IlluCSharp/UsingStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace UsingStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (TextWriter tw = File.CreateText("Lincoln.txt"))
+            {
+                tw.WriteLine("Four score and ");
+            }
+
+            using (TextReader tr = File.OpenText("Lincoln.txt"))
+            {
+                string InputString;
+
+                while (null != (InputString = tr.ReadLine()))
+                {
+                    Console.WriteLine(InputString);
+                }
+            }
+
+
+        }
+    }
+}
