3165cf6e2b59a8cfbc6f499467550ec3e9f62840
yuliang <109827298@qq.com>
2017-09-26 17:24:55 +0800

    add files

---
+++ IlluCSharp/AssigningStruct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigningStruct
+{
+    struct Simple
+    {
+        public int X;
+        public int Y;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simple ss1;
+            Simple ss2;
+
+            ss1.X = 5;
+            ss1.Y = 10;
+
+            ss2 = ss1;
+
+            Console.WriteLine("here");
+        }
+    }
+}
