1f86ecc3a3b9bb98ac5ce86b05385193493ee173
yuliang <109827298@qq.com>
2017-09-27 16:12:51 +0800

    add files

---
+++ IlluCSharp/MoreEnums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreEnums
+{
+    //enum FirstEnum
+    //{
+    //    Mem1,
+    //    Mem2
+    //}
+
+    //enum SecondEnum
+    //{
+    //    Mem1,
+    //    Mem2
+    //}
+
+    enum TrafficLight
+    {
+        Green,
+        Yellow,
+        Red
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (FirstEnum.Mem1 < FirstEnum.Mem2)
+            //{
+            //    Console.WriteLine("True");
+            //}
+
+            //if (FirstEnum.Mem1 < SecondEnum.Mem1)
+            //{
+            //    Console.WriteLine("True");
+            //}
+
+            Console.WriteLine(Enum.GetName(typeof(TrafficLight), 0));
+
+            foreach (var name in Enum.GetNames(typeof(TrafficLight)))
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
