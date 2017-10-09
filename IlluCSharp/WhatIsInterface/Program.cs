d7138ea664f35ced785d4423ca4cc465fa7383e2
YuLiang <109827298@qq.com>
2017-10-10 07:44:20 +0800

    add fles

---
+++ IlluCSharp/WhatIsInterface/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatIsInterface
+{
+    interface IInfo
+    {
+        string GetName();
+        string GetAge();
+    }
+
+    class CA:IInfo
+    {
+        public string Name;
+        public int Age;
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public string GetAge()
+        {
+            return Age.ToString();
+        }
+    }
+
+    class CB:IInfo
+    {
+        public string First;
+        public string Last;
+        public double PersonsAge;
+
+        public string GetName()
+        {
+            return First + " " + Last;
+        }
+
+        public string GetAge()
+        {
+            return PersonsAge.ToString();
+        }
+    }
+
+
+    class Program
+    {
+        static void PrintInfo(IInfo item)
+        {
+            Console.WriteLine("Name: {0}, Age {1}", 
+                item.GetName(), 
+                item.GetAge());
+
+        }
+
+        static void Main(string[] args)
+        {
+            CA a = new CA()
+            {
+                Name = "John Doe",
+                Age = 35
+            };
+
+            CB b = new CB()
+            {
+                First = "Jane",
+                Last = "Doe",
+                PersonsAge = 33
+            };
+
+            PrintInfo(a);
+            PrintInfo(b);
+        }
+    }
+}
