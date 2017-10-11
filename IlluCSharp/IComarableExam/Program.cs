b274b73cf141a45bc2984dc4fddb4eaab171e9b4
yuliang <109827298@qq.com>
2017-10-11 10:06:17 +0800

    add files

---
+++ IlluCSharp/IComarableExam/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComarableExam
+{
+    class MyClass : IComparable
+    {
+        public int TheValue;
+
+        public int CompareTo(object obj)
+        {
+            MyClass mc = (MyClass)obj;
+
+            // 对象的值和参数的值做比较.
+            if (this.TheValue < mc.TheValue)
+            {
+                return -1;
+            }
+            else if (this.TheValue > mc.TheValue)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void PrintOut(string s, MyClass[] mc)
+        {
+            Console.Write(s);
+
+            foreach (var m in mc)
+            {
+                Console.Write("{0} ", m.TheValue);
+            }
+
+            Console.WriteLine("");
+        }
+
+        static void Main(string[] args)
+        {
+            var myInt = new[] { 20, 4, 16, 9, 2 };
+
+            MyClass[] mcArr = new MyClass[5];
+
+            for (int i = 0; i < myInt.Length; i++)
+            {
+                mcArr[i] = new MyClass();
+                mcArr[i].TheValue = myInt[i];
+            }
+
+            PrintOut("Initial Order: ", mcArr);
+
+            Array.Sort(mcArr);
+
+            PrintOut("Sorted Order: ", mcArr);
+        }
+    }
+}
--- IlluCSharp/RaisingEvent/Program.cs
+++ IlluCSharp/RaisingEvent/Program.cs
@@ -7,22 +7,32 @@ using System.Threading.Tasks;
 namespace RaisingEvent
 {
     // Declare the delegate
-    delegate void Handler();
+    //delegate void Handler();
+
+    public class IncrementerEventArgs : EventArgs
+    {
+        public int IterationCount { get; set; }
+    }
 
     // Publisher
     class Incrementer
     {
         // Create and publish an event.
-        public event Handler CountedADozen;
+        //public event Handler CountedADozen;
+        //public event EventHandler CountedADozen;
+        public event EventHandler<IncrementerEventArgs> CountedADozen;
 
         public void DoCount()
         {
+            IncrementerEventArgs args = new IncrementerEventArgs();
+
             for (int i = 1; i < 100; i++)
             {
                 if (i % 12 == 0 && CountedADozen != null)
                 {
+                    args.IterationCount = i;
                     // Raise the event
-                    CountedADozen();
+                    CountedADozen(this, args);
                 }
             }
         }
@@ -43,8 +53,10 @@ namespace RaisingEvent
             incrementer.CountedADozen += IncremenDozensCount;
         }
 
-        void IncremenDozensCount()
+        void IncremenDozensCount(object source, IncrementerEventArgs e)
         {
+            Console.WriteLine("Incremented at iteration: {0} in {1}", e.IterationCount, source.ToString());
+
             DozensCount++;
         }
     }
