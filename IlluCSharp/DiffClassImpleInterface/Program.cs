172c31e3d8d5651aa15731b42d25717677f34344
yuliang <109827298@qq.com>
2017-10-11 15:44:56 +0800

    add files

---
+++ IlluCSharp/DiffClassImpleInterface/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffClassImpleInterface
+{
+    interface ILiveBirth
+    {
+        string BabyCalled();
+    }
+
+    class Animal { }
+
+    class Cat : Animal, ILiveBirth
+    {
+        string ILiveBirth.BabyCalled()
+        {
+            return "kitten";
+        }
+    }
+
+    class Dog : Animal, ILiveBirth
+    {
+        string ILiveBirth.BabyCalled()
+        {
+            return "puppy";
+        }
+    }
+
+    class Bird : Animal 
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animalArray = new Animal[3];
+
+            animalArray[0] = new Cat();
+            animalArray[1] = new Bird();
+            animalArray[2] = new Dog();
+
+            foreach (Animal a in animalArray)
+            {
+                ILiveBirth b = a as ILiveBirth;
+                if (b != null)
+                {
+                    Console.WriteLine("Baby is called: {0}", b.BabyCalled());
+                }
+            }
+        
+        }
+    }
+}
---
+++ IlluCSharp/InterfacesInheritInterfaces/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesInheritInterfaces
+{
+    interface IDataRetrieve
+    {
+        int GetData();
+    }
+
+    interface IDataStore
+    {
+        void SetData(int x);
+    }
+
+    interface IDataIO : IDataRetrieve, IDataStore
+    { 
+    }
+
+    class MyData : IDataIO
+    {
+        int nPrivateData;
+
+        public int GetData()
+        {
+            return nPrivateData;
+        }
+
+        public void SetData(int x)
+        {
+            nPrivateData = x;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyData data = new MyData();
+
+            data.SetData(5);
+
+            Console.WriteLine("{0}", data.GetData());
+        }
+    }
+}
