8ad48eaa845a4b270662a87029655e712eade5fc
yuliang <109827298@qq.com>
2017-10-12 15:17:21 +0800

    add files

--- IlluCSharp/RefConversions/Program.cs
+++ IlluCSharp/RefConversions/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace RefConversions
 {
+    class A
+    {
+        public int Field1;
+    }
+
+    class B : A 
+    {
+        public int Field2;
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            //B myVar1 = new B();
+
+            //A myVar2 = (A)myVar1;
+
+            //Console.WriteLine("{0}", myVar2.Field1);
+            ////Console.WriteLine("{0}", myVar2.Field2);
+
+            //A myVar1 = new A();
+
+            //B myVar2 = (B)myVar1;
+
+
+
+            B myVar1 = new B();
+            A myVar2 = myVar1;
+            B myVar3 = (B)myVar2;
         }
     }
 }
