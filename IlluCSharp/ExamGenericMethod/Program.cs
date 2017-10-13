b3abd54f75e78aefdf5d62890c1dc0e9f9de034b
yuliang <109827298@qq.com>
2017-10-13 18:46:06 +0800

    add files

---
+++ IlluCSharp/ExamGenericMethod/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamGenericMethod
+{
+    class Simple
+    {
+        static public void ReverseAndPrint<T>(T[] arr)
+        {
+            Array.Reverse(arr);
+
+            foreach (T item in arr)
+            {
+                Console.WriteLine("{0}, ", item.ToString());
+            }
+
+            Console.WriteLine("");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intArray = new int[] { 3, 5, 7, 9, 11 };
+
+            var stringArray = new string[] 
+            {
+                "first",
+                "second",
+                "third"
+            };
+
+            var doubleArray = new double[] { 3.567, 7.891, 2.345 };
+
+            Simple.ReverseAndPrint<int>(intArray);
+            Simple.ReverseAndPrint(intArray);
+
+            Simple.ReverseAndPrint<string>(stringArray);
+            Simple.ReverseAndPrint(stringArray);
+
+            Simple.ReverseAndPrint<double>(doubleArray);
+            Simple.ReverseAndPrint(doubleArray);
+        }
+    }
+}
--- IlluCSharp/StackExamGenerics/Program.cs
+++ IlluCSharp/StackExamGenerics/Program.cs
@@ -6,80 +6,89 @@ using System.Threading.Tasks;
 
 namespace StackExamGenerics
 {
-    class MyStack<T>
+    //class MyStack<T>
+    //{
+    //    T[] StackArray;
+
+    //    // 为0时 没有元素 指向栈顶的位置.
+    //    int StackPointer = 0;
+
+    //    const int MaxStack = 10;
+    //    bool IsStackFull
+    //    {
+    //        get
+    //        {
+    //            return StackPointer >= MaxStack;
+    //        }
+    //    }
+
+    //    bool IsStackEmpty
+    //    {
+    //        get
+    //        {
+    //            return StackPointer <= 0;
+    //        }
+    //    }
+
+    //    public void Push(T x)
+    //    {
+    //        if (!IsStackFull)
+    //        {
+    //            // 改变栈顶位置 之后把指针+1
+    //            StackArray[StackPointer++] = x;
+    //        }
+    //    }
+
+    //    public T Pop()
+    //    {
+    //        // 出栈时。
+    //        // 如果不为空 StackPointer至少为1
+
+    //        // 如果没有元素时 返回一个默认值？
+    //        // 出栈时有个问题 只是返回了相应的元素 没有把栈里元素剔除.
+
+    //        return (!IsStackEmpty) ? StackArray[--StackPointer] : StackArray[0];
+    //    }
+
+    //    public MyStack()
+    //    {
+    //        StackArray = new T[MaxStack];
+    //    }
+
+    //    public void Print()
+    //    {
+    //        for (int i = StackPointer-1; i >= 0; i--)
+    //        {
+    //            Console.WriteLine("    Value: {0}", StackArray[i]);
+    //        }
+    //    }
+    //}
+
+    class Simple<T>
+        where T : class
     {
-        T[] StackArray;
-
-        // 为0时 没有元素 指向栈顶的位置.
-        int StackPointer = 0;
-
-        const int MaxStack = 10;
-        bool IsStackFull
-        {
-            get
-            {
-                return StackPointer >= MaxStack;
-            }
-        }
-
-        bool IsStackEmpty
-        {
-            get
-            {
-                return StackPointer <= 0;
-            }
-        }
-
-        public void Push(T x)
-        {
-            if (!IsStackFull)
-            {
-                // 改变栈顶位置 之后把指针+1
-                StackArray[StackPointer++] = x;
-            }
-        }
-
-        public T Pop()
-        {
-            // 出栈时。
-            // 如果不为空 StackPointer至少为1
-
-            // 如果没有元素时 返回一个默认值？
-            // 出栈时有个问题 只是返回了相应的元素 没有把栈里元素剔除.
-
-            return (!IsStackEmpty) ? StackArray[--StackPointer] : StackArray[0];
-        }
-
-        public MyStack()
-        {
-            StackArray = new T[MaxStack];
-        }
-
-        public void Print()
-        {
-            for (int i = StackPointer-1; i >= 0; i--)
-            {
-                Console.WriteLine("    Value: {0}", StackArray[i]);
-            }
-        }
+        //static public bool LessThan(T i1, T i2)
+        //{
+        //    return i1 < i2;
+        //}
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            MyStack<int> StackInt = new MyStack<int>();
-            MyStack<string> StackString = new MyStack<string>();
+            //MyStack<int> StackInt = new MyStack<int>();
+            //MyStack<string> StackString = new MyStack<string>();
 
-            StackInt.Push(5);
+            //StackInt.Push(5);
 
 
 
-            int nCheck = StackInt.Pop();
-            Console.WriteLine("nCheck = {0}", nCheck);
+            //int nCheck = StackInt.Pop();
+            //Console.WriteLine("nCheck = {0}", nCheck);
 
-            nCheck = StackInt.Pop();
-            Console.WriteLine("nCheck = {0}", nCheck);
+            //nCheck = StackInt.Pop();
+            //Console.WriteLine("nCheck = {0}", nCheck);
 
             //StackInt.Push(3);
             //StackInt.Push(5);
