d0660b49a3ac9249b1b2fa55e380ab6b19a10c5d
YuLiang <109827298@qq.com>
2017-10-14 08:16:11 +0800

    add files

---
+++ IlluCSharp/Covariance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covariance
+{
+    class Animal
+    {
+        public int NumberOfLegs = 4;
+    }
+
+    class Dog : Animal
+    {
+        public int nDog;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a1 = new Animal();
+            Animal a2 = new Dog();
+
+            Console.WriteLine("Number of dog legs: {0}", a2.NumberOfLegs);
+        }
+    }
+}
---
+++ IlluCSharp/GenericInterfaces/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericInterfaces
+{
+    interface IMyIfc<T>
+    {
+        T ReturnIt(T inValue);
+    }
+
+    //class Simple<S> : IMyIfc<S>
+    //{
+    //    public S ReturnIt(S inValue)
+    //    {
+    //        return inValue;
+    //    }
+    //}
+
+    //class Simple : IMyIfc<int>, IMyIfc<string>
+    //{
+    //    public int ReturnIt(int inValue)
+    //    {
+    //        return inValue;
+    //    }
+
+    //    public string ReturnIt(string inValue)
+    //    {
+    //        return inValue;
+    //    }
+    //}
+
+    //class Simple<S> : IMyIfc<int>, IMyIfc<S>
+    //{
+ 
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var trivInt = new Simple<int>();
+            //var trivString = new Simple<string>();
+            //Simple trivial = new Simple();
+
+            //Console.WriteLine("{0}", trivInt.ReturnIt(5));
+            //Console.WriteLine("{0}", trivString.ReturnIt("Hi there"));
+
+            //Console.WriteLine("{0}", trivial.ReturnIt(5));
+            //Console.WriteLine("{0}", trivial.ReturnIt("Hi there."));
+        }
+    }
+}
