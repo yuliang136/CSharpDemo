227cef9489d6e0d39c54d16161dbaf89c22bb540
yuliang <109827298@qq.com>
2017-10-25 18:40:57 +0800

    add files

---
+++ IlluCSharp/ExamNestedClass/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamNestedClass
+{
+    class MyClass
+    {
+        class MyCounter
+        {
+            public int Count
+            {
+                get;
+                private set;
+            }
+
+            public static MyCounter operator ++(MyCounter current)
+            {
+                current.Count++;
+
+                return current;
+            }
+        }
+
+        private MyCounter counter;
+
+        public MyClass()
+        {
+            counter = new MyCounter();
+        }
+
+        public int Incr()
+        {
+            return (counter++).Count;
+        }
+
+        public int GetValue()
+        {
+            return counter.Count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+
+            mc.Incr();
+            mc.Incr();
+
+            Console.WriteLine("Total: {0}", mc.GetValue());
+        }
+    }
+}
---
+++ IlluCSharp/Visibility and Nested Types/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visibility_and_Nested_Types
+{
+    class SomeClass
+    {
+        int Field1 = 15;
+        int Field2 = 20;
+
+        MyNested mn = null;
+
+        public void PrintMyMembers()
+        {
+            mn.PrintOuterMembers();
+        }
+
+        public SomeClass()
+        {
+            mn = new MyNested(this);
+        }
+
+        class MyNested
+        {
+            SomeClass sc = null;
+
+            public MyNested(SomeClass SC)
+            {
+                sc = SC;
+            }
+
+            public void PrintOuterMembers()
+            {
+                Console.WriteLine("Field1: {0}", sc.Field1);
+                Console.WriteLine("Field2: {0}", sc.Field2);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SomeClass MySC = new SomeClass();
+
+            MySC.PrintMyMembers();
+        }
+    }
+}
