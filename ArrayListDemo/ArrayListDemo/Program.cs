eadbb5b0c03329866f98af4342f620835a556ed0
yuliang <109827298@qq.com>
2017-11-10 14:13:37 +0800

    add files

---
+++ ArrayListDemo/ArrayListDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ulong m_Max = 50000000;
+
+            ArrayList list = new ArrayList();
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            for (ulong i = 0; i < m_Max; i++)
+            {
+                list.Add(i);
+            }
+
+            watch.Stop();
+            Console.WriteLine("ArrayList Time : " + watch.Elapsed);
+
+
+            //Stopwatch watch = new Stopwatch();
+            //List<ulong> uLongList = new List<ulong>();
+            //watch.Start();
+            //for (ulong i = 0; i < m_Max; i++)
+            //{
+            //    uLongList.Add(i);
+            //}
+            //watch.Stop();
+            //Console.WriteLine("List<T> Time : " + watch.Elapsed);
+
+        }
+    }
+}
---
+++ StringDemo/StringDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            //string strHandle = @"F:\Lzyy115\Lzyy115FullPack\lzyy_1150_20170906\Assets\Resources\Assets\csv\achieve.bytes";
+
+
+            string strHandle = @"csv\achieve.bytes";
+            //configList[i] = configList[i].Substring(0, configList[i].LastIndexOf('.'));
+
+            // 获得索引值.
+            // 索引从0开始计数.
+            // 当索引返回81时，从该索引到开头的字符个数应该+1
+            int nLength = strHandle.LastIndexOf('.');
+
+            //int nLength = strHandle.IndexOf('.');
+
+            Console.WriteLine(nLength);
+
+
+            // Length从1开始计数.
+            // 这里直接用索引值当长度用, 相当于把找到的字符省略掉
+            string strGet = strHandle.Substring(0, nLength);
+        }
+    }
+}
