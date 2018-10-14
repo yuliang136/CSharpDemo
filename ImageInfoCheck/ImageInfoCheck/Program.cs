f552be3c541fee7d9b89d86fe1073f2ef4b518f1
YuLiang <109827298@qq.com>
2018-10-14 21:22:49 +0800

    添加ImageInfoCheck

---
+++ ImageInfoCheck/ImageInfoCheck/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageInfoCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Check");
+
+            string strText1 = @"AllIconsAtlas.png";
+            string strText2 = @"loading00.png";
+
+            using (FileStream fs = new FileStream(strText2, FileMode.Open, FileAccess.Read))
+            {
+                Image image = Image.FromStream(fs);
+
+                PixelFormat check = image.PixelFormat;
+
+                Console.WriteLine(check.ToString());
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CSharpDemo
 CSharpDemo
+
+ImageInfoCheck 检查图片格式 区分透明通道和非透明通道
