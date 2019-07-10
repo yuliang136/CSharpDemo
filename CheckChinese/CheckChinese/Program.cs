b1552bf6d28fdeb787d92bd67edff07bd962e8c8
moon <109827298@qq.com>
2019-07-10 12:51:38 +0800

    add files

---
+++ CheckChinese/CheckChinese/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckChinese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check Chinese");
+
+            DirectoryInfo di = new DirectoryInfo(@"G:\GitBranches\CSharpDemo\CheckChinese\CheckChinese\bin\Debug\38918\StreamingAssets");
+
+            FileInfo[] files = di.GetFiles();
+
+            foreach (var eachFile in files)
+            {
+                //Console.WriteLine(eachFile.Name);
+
+                if (CheckStringChineseReg(eachFile.Name))
+                {
+                    Console.WriteLine(eachFile.Name);
+                }
+
+            }
+
+            
+        }
+
+        public static bool CheckStringChineseReg(string text)
+        {
+            return System.Text.RegularExpressions.Regex.IsMatch(text, @"[\u4e00-\u9fbb]");
+        }
+    }
+}
