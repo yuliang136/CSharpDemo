edd3bec2896cc7c0605230a96ecd2b7aa93ac710
YuLiang <109827298@qq.com>
2020-10-28 20:11:46 +0800

    add fies

--- FindRefsMulti/FindRefsMulti/Program.cs
+++ FindRefsMulti/FindRefsMulti/Program.cs
@@ -13,13 +13,13 @@ namespace FindRefsMulti
         {
             StopWatchUtils.WatchStopInit();
             // 取得Assets目录.
-            string strCheckFilesPath = @"C:\gitWorks\gitbranch_ios_pack\Assets\AssetBundleReporter";
+            string strCheckFilesPath = @"C:\gitWorks\gitbranch_ios_pack\Assets\UTNotifications";
             string[] checkFiles = Directory.GetFiles(strCheckFilesPath, "*.*", SearchOption.AllDirectories);
             List<string> totalLst = checkFiles.ToList();                            // Check文件列表
             string strDepFilesPath = @"C:\gitWorks\gitbranch_ios_pack\Assets";      // Dep文件查找目录.
             string strUnityAssetPath = @"C:\gitWorks\gitbranch_ios_pack\Assets";    // Unity Assets固定目录.
-            string strOutputFilePath = @"D:\checkAssetBundleReporter.json";         // 输出文件名字
-            int nThreadNum = 1;
+            string strOutputFilePath = @"D:\checkUTNotifications.json";         // 输出文件名字
+            int nThreadNum = 200;
 
 
             FindRefWithMulti findRefWithMulti = new FindRefWithMulti(
