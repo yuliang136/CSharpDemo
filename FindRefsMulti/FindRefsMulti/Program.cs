dc519ce84631b40a71038842639ae2e359e0d6cf
YuLiang <109827298@qq.com>
2020-11-05 22:03:45 +0800

    add files

--- FindRefsMulti/FindRefsMulti/Program.cs
+++ FindRefsMulti/FindRefsMulti/Program.cs
@@ -11,30 +11,11 @@ namespace FindRefsMulti
     {
         static void Main(string[] args)
         {
-            StopWatchUtils.WatchStopInit();
-            // 取得Assets目录.
-            string strCheckFilesPath = @"C:\gitWorks\gitbranch_ios_pack\Assets\UTNotifications";
-            string[] checkFiles = Directory.GetFiles(strCheckFilesPath, "*.*", SearchOption.AllDirectories);
-            List<string> totalLst = checkFiles.ToList();                            // Check文件列表
-            string strDepFilesPath = @"C:\gitWorks\gitbranch_ios_pack\Assets";      // Dep文件查找目录.
-            string strUnityAssetPath = @"C:\gitWorks\gitbranch_ios_pack\Assets";    // Unity Assets固定目录.
-            string strOutputFilePath = @"D:\checkUTNotifications.json";         // 输出文件名字
-            int nThreadNum = 200;
-
-
-            FindRefWithMulti findRefWithMulti = new FindRefWithMulti(
-                                                        strDepFilesPath, 
-                                                        totalLst, 
-                                                        strOutputFilePath, 
-                                                        strUnityAssetPath,
-                                                        nThreadNum);
-            findRefWithMulti.Run();
-            StopWatchUtils.WatchStopOnceEnd("FindRefWithMulti Handle Done!");
+            // 测试FindRefWithMulti
+            // TestFindRefWithMulti.Run();
 
 
 
-            // AnalyzeTools.CheckFileExts(strApplicationDataPath);
-            Console.ReadKey();
         }
     }
 }
---
+++ FindRefsMulti/FindRefsMulti/TestFindRefWithMulti.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FindRefsMulti
+{
+    public static class TestFindRefWithMulti
+    {
+        public static void Run()
+        {
+            StopWatchUtils.WatchStopInit();
+            // 取得Assets目录.
+            string strCheckFilesPath = @"C:\svnWorks\android142_cjp_pack\Assets";
+            string[] checkFiles = Directory.GetFiles(strCheckFilesPath, "*.*", SearchOption.AllDirectories);
+            List<string> totalLst = checkFiles.ToList();                            // Check文件列表
+            string strDepFilesPath = @"C:\svnWorks\android142_cjp_pack\Assets";      // Dep文件查找目录.
+            string strUnityAssetPath = @"C:\svnWorks\android142_cjp_pack\Assets";    // Unity Assets固定目录.
+            string strOutputFilePath = @"D:\check_cjp.json";         // 输出文件名字
+            int nThreadNum = 200;
+
+
+            FindRefWithMulti findRefWithMulti = new FindRefWithMulti(
+                strDepFilesPath,
+                totalLst,
+                strOutputFilePath,
+                strUnityAssetPath,
+                nThreadNum);
+            findRefWithMulti.Run();
+            StopWatchUtils.WatchStopOnceEnd("FindRefWithMulti Handle Done!");
+
+
+
+            // AnalyzeTools.CheckFileExts(strApplicationDataPath);
+            Console.ReadKey();
+        }
+    }
+}
