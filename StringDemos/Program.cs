c0b839b7ac596b93a49593a9f4f44bf28fc77b2a
YuLiang <109827298@qq.com>
2021-05-10 14:54:17 +0800

    add files

---
+++ StringDemos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StringDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string EndsWith的方法.
+            string strTest = "luaprefab_m20003_uiprefab_whatsnewspanel.unity3d";
+            string strFind = "luaprefab_m20003_uiprefab_whatsnewspanel.unity3d";
+
+            bool bFindString = strTest.EndsWith(strFind);
+
+            Console.WriteLine("Check Here");
+
+        }
+    }
+}
