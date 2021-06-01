473a0d72bd9debbcae1eed1c3c0f75e835a009cd
DESKTOP-RAV4DNM\99 <109827298@qq.com>
2021-06-01 10:55:12 +0800

    添加默认访问符

---
+++ DefaultModifier/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefaultModifier
+{
+
+    /// <summary>
+    /// 这里类的默认修饰符应该是public?
+    /// </summary>
+    class TestA
+    {
+        // 这里必须显式设置
+        public int testa;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            TestA a = new TestA();
+            a.testa = 100;
+
+
+        }
+    }
+}
